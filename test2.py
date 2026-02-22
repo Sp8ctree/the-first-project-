8a35f08c1758f7d64e050f2b84e198c4c7da85f0
Ivanchk4 <ivan208koza@gmail.com>
2026-02-22 21:12:48 +0400

    Теперь кроме строчных чисел этот код умеет сдвигать цифры и заглавные буквы

--- test2.py
+++ test2.py
@@ -3,5 +3,7 @@ res = []
 slovo = input('Что кодируем?')
 sdvig = int(input("Каков сдвиг?"))
 for i in slovo:
-    res.append(ascii_lowercase[slovo.index(i)+sdvig])
+    if i in ascii_uppercase:res.append(ascii_uppercase[(slovo.index(i) + sdvig)%(len(ascii_uppercase)-1)])
+    if i in ascii_lowercase:res.append(ascii_lowercase[(slovo.index(i) + sdvig)%(len(ascii_lowercase)-1)])
+    if i in digits:res.append((digits[(slovo.index(i) + sdvig+1)%(len(digits))]))
 print(''.join(res))
