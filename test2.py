c13aaa73d4399a64e9da576aff505d78c43724ba
Ivanchk4 <ivan208koza@gmail.com>
2026-02-22 21:32:57 +0400

    Добавлен Могучий Русский Язык
    -исправлены баги прошлой версии
    -добавлен русский язык

--- test2.py
+++ test2.py
@@ -1,9 +1,14 @@
 from string import *
 res = []
+rus_uppercase = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+rus_lowercase = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 slovo = input('Что кодируем?')
 sdvig = int(input("Каков сдвиг?"))
 for i in slovo:
-    if i in ascii_uppercase:res.append(ascii_uppercase[(slovo.index(i) + sdvig)%(len(ascii_uppercase)-1)])
-    if i in ascii_lowercase:res.append(ascii_lowercase[(slovo.index(i) + sdvig)%(len(ascii_lowercase)-1)])
-    if i in digits:res.append((digits[(slovo.index(i) + sdvig+1)%(len(digits))]))
+    if i in ascii_lowercase:res.append(ascii_lowercase[(ascii_lowercase.index(i)+sdvig)%26])
+    if i in ascii_uppercase: res.append(ascii_uppercase[(ascii_uppercase.index(i) + sdvig) % 26])
+    if i in rus_uppercase:res.append(rus_uppercase[(rus_uppercase.index(i)+sdvig)%26])
+    if i in rus_lowercase: res.append(rus_lowercase[(rus_lowercase.index(i) + sdvig) % 26])
+    if i in digits: res.append(digits[(digits.index(i) + sdvig) % 10])
 print(''.join(res))
+
